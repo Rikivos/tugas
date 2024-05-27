ed04142405fa5f1c174978116eaa9a4a7202b4a8
rizkypenpenz <rizkypenpenz@gmail.com>
2024-05-27 01:23:50 +0000

    masukan pesan perubahanya

--- app.py
+++ app.py
@@ -56,24 +56,63 @@ def user(username):
 
 @app.route("/sign_in", methods=["POST"])
 def sign_in():
-    # an api endpoint for logging in
-    return jsonify({"result": "success"})
+    # Sign in
+    username_receive = request.form["username_give"]
+    password_receive = request.form["password_give"]
+    pw_hash = hashlib.sha256(password_receive.encode("utf-8")).hexdigest()
+    result = db.users.find_one(
+        {
+            "username": username_receive,
+            "password": pw_hash,
+        }
+    )
+    if result:
+        payload = {
+            "id": username_receive,
+            # the token will be valid for 24 hours
+            "exp": datetime.utcnow() + timedelta(seconds=60 * 60 * 24),
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+        return jsonify(
+            {
+                "result": "success",
+                "token": token,
+            }
+        )
+    # Let's also handle the case where the id and
+    # password combination cannot be found
+    else:
+        return jsonify(
+            {
+                "result": "fail",
+                "msg": "We could not find a user with that id/password combination",
+            }
+        )
 
 
 @app.route("/sign_up/save", methods=["POST"])
 def sign_up():
-    # an api endpoint for signing up
-    username_receive = request.form["username_give"]
-    password_receive = request.form["password_give"]
-    password_hash = hashlib.sha256(password_receive.encode("utf-8")).hexdigest()
-    # we should save the user to the database
-    return jsonify({"result": "success"})
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # id
+        "password": password_hash,                                  # password
+        "profile_name": username_receive,                           # user's name is set to their id by default
+        "profile_pic": "",                                          # profile image file name
+        "profile_pic_real": "profile_pics/profile_placeholder.png", # a default profile image
+        "profile_info": ""                                          # a profile description
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
 
 
-@app.route("/sign_up/check_dup", methods=["POST"])
+@app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
-    # ID we should check whether or not the id is already taken
-    return jsonify({"result": "success"})
+    username_receive = request.form['username_give']
+    exists = bool(db.user.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 
 @app.route("/update_profile", methods=["POST"])
@@ -124,6 +163,7 @@ def about():
 @app.route("/secret", methods=["GET"])
 def secret():
     return render_template('secret.html')
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5001, debug=True)
